bbfe0ae19c8dae85c823a71e5eed7f2e0d2873b9
정종인 <chongin12@naver.com>
2022-07-20 18:34:43 +0900

    OTT ios

--- ott/ViewController.swift
+++ ott/ViewController.swift
@@ -6,14 +6,98 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
+import ReplayKit
 
-class ViewController: UIViewController {
-
+class ViewController: RPBroadcastActivityViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let broadcastPicker = RPSystemBroadcastPickerView(frame: CGRect(x: 100, y: 200, width: 50, height: 50))
+        broadcastPicker.preferredExtension = "com.chongin12.dev.ott.record"
+        
+        self.view.addSubview(broadcastPicker)
     }
-
-
+    
+    @IBAction func onTap(_ sender: UIButton) {
+        read()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    private func read() {
+        print("read")
+        let fileManager = FileManager.default
+        var mediaURLs: [URL] = []
+        if let container = fileManager
+            .containerURL(
+                forSecurityApplicationGroupIdentifier: "group.chongin12.dev"
+            )?.appendingPathComponent("Library/") {
+            
+            let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+            do {
+                let contents = try fileManager.contentsOfDirectory(atPath: container.path)
+                print("contents : \(contents)")
+                for path in contents {
+                    if path.elementsEqual("Caches") {
+                        continue
+                    }
+                    if !path.hasSuffix(".mp4") {
+                        continue
+                    }
+//                    guard !path.hasSuffix(".plist") else {
+//                        print("file at path \(path) is plist, exiting")
+//                        return
+//                    }
+                    let fileURL = container.appendingPathComponent(path)
+                    var isDirectory: ObjCBool = false
+                    guard fileManager.fileExists(atPath: fileURL.path, isDirectory: &isDirectory) else {
+                        return
+                    }
+                    guard !isDirectory.boolValue else {
+                        return
+                    }
+                    let destinationURL = documentsDirectory.appendingPathComponent(path)
+                    do {
+                        try fileManager.moveItem(at: fileURL, to: destinationURL)
+                        print("Successfully moved \(fileURL)", "to: ", destinationURL)
+                    } catch {
+                        print("error moving \(fileURL) to \(destinationURL)", error)
+                    }
+                    mediaURLs.append(destinationURL)
+                    print("mediaURLs : \(mediaURLs)")
+                }
+            } catch {
+                print("contents, \(error)")
+            }
+        } else {
+            print("if let fail")
+        }
+        
+        mediaURLs.last.map {
+            print("mediaURLs.last.map")
+            let asset: AVURLAsset = .init(url: $0)
+            let item: AVPlayerItem = .init(asset: asset)
+            
+            let movie: AVMutableMovie = .init(url: $0)
+            for track in movie.tracks {
+                print("track", track)
+            }
+            
+            let player: AVPlayer = .init(playerItem: item)
+            let playerViewController: AVPlayerViewController = .init()
+            playerViewController.player = player
+            playerViewController.modalPresentationStyle = .fullScreen
+            self.present(playerViewController, animated: true, completion: { [player = playerViewController.player] in
+                player?.play()
+            })
+        }
+    }
 }
-
---
+++ record/SampleHandler.swift
@@ -0,0 +1,133 @@
+//
+//  SampleHandler.swift
+//  record
+//
+//  Created by 정종인 on 2022/07/19.
+//
+
+import ReplayKit
+
+class SampleHandler: RPBroadcastSampleHandler {
+
+    private var writer: BroadcastWriter?
+    private let fileManager: FileManager = .default
+    private let nodeURL: URL
+
+    override init() {
+        print("init")
+        nodeURL = fileManager.temporaryDirectory
+            .appendingPathComponent(UUID().uuidString)
+            .appendingPathExtension(for: .mpeg4Movie)
+
+        fileManager.removeFileIfExists(url: nodeURL)
+        do {
+            let url = (fileManager.containerURL(
+                forSecurityApplicationGroupIdentifier: "group.chongin12.dev"
+            )?.appendingPathComponent("Library/"))!
+            try fileManager.removeItem(at: url)
+        } catch { }
+
+        super.init()
+    }
+
+    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+        print("started")
+        let screen: UIScreen = .main
+        do {
+            writer = try .init(
+                outputURL: nodeURL,
+                screenSize: screen.bounds.size,
+                screenScale: screen.scale
+            )
+        } catch {
+            assertionFailure(error.localizedDescription)
+            finishBroadcastWithError(error)
+            return
+        }
+        do {
+            try writer?.start()
+        } catch {
+            finishBroadcastWithError(error)
+        }
+        print("started fin")
+    }
+
+    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
+        guard let writer = writer else {
+            debugPrint("processSampleBuffer: Writer is nil")
+            return
+        }
+
+        do {
+            let captured = try writer.processSampleBuffer(sampleBuffer, with: sampleBufferType)
+            debugPrint("processSampleBuffer captured", captured)
+        } catch {
+            debugPrint("processSampleBuffer error:", error.localizedDescription)
+        }
+    }
+
+    override func broadcastPaused() {
+        debugPrint("=== paused")
+        writer?.pause()
+    }
+
+    override func broadcastResumed() {
+        debugPrint("=== resumed")
+        writer?.resume()
+    }
+
+    override func broadcastFinished() {
+        print("broadcast finished")
+        guard let writer = writer else {
+            return
+        }
+
+        let outputURL: URL
+        do {
+            outputURL = try writer.finish()
+        } catch {
+            debugPrint("writer failure", error)
+            return
+        }
+
+        guard let containerURL = fileManager.containerURL(
+                    forSecurityApplicationGroupIdentifier: "group.chongin12.dev"
+        )?.appendingPathComponent("Library/") else {
+            fatalError("no container directory")
+        }
+        do {
+            try fileManager.createDirectory(
+                at: containerURL,
+                withIntermediateDirectories: true,
+                attributes: nil
+            )
+        } catch {
+            debugPrint("error creating", containerURL, error)
+        }
+
+        let destination = containerURL.appendingPathComponent(outputURL.lastPathComponent)
+        do {
+            debugPrint("Moving", outputURL, "to:", destination)
+            try self.fileManager.moveItem(
+                at: outputURL,
+                to: destination
+            )
+        } catch {
+            debugPrint("ERROR", error)
+        }
+
+        debugPrint("FINISHED")
+    }
+}
+
+extension FileManager {
+
+    func removeFileIfExists(url: URL) {
+        guard fileExists(atPath: url.path) else { return }
+        do {
+            try removeItem(at: url)
+        } catch {
+            print("error removing item \(url)", error)
+        }
+    }
+}
